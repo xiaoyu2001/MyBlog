132db140e966038cf6c012213baf202a5938a81d
xiaoyu2001 <66050231@qq.com>
2020-08-13 18:38:57 +0800

    更新配置config.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'iview/dist/styles/iview.css'
-import './assets/iview/css/index.less';
+/*import './assets/iview/css/index.less'*/
 import animate from 'animate.css'
 
 Vue.use(animate)
